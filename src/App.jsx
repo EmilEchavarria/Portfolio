d2d2b01dafd4cec397a9a80416b8ccc7fe338f50
EmilEchavarria <159707410+EmilEchavarria@users.noreply.github.com>
2025-03-20 11:53:04 -0400

    Switch to HashRouter for routing to avoid 404 on refresh

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom"; 
+import { HashRouter as Router, Route, Routes } from "react-router-dom"; 
 import { RetroGrid } from './components/ui/retro-grid'; 
 import { Home } from './pages/home/home'; 
 import About from './pages/about/about'; 
@@ -6,11 +6,6 @@ import Projects from './pages/projects/projects';
 import Education from './pages/education/education'; 
 import Certificates from './pages/certificates/certificates'; 
 import Contact from './pages/contact/contact'; 
-
-
-
-
-
 
 function App() {
   return (
@@ -31,11 +26,6 @@ function App() {
               <Route path="/education" element={<Education/>} />
               <Route path="/certificates" element={<Certificates/>} />
               <Route path="/contact" element={<Contact/>} />
-
-
-
-            
-
             </Routes>
           </div>
         </div>
